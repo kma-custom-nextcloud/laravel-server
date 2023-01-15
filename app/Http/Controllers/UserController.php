018b5cc8cdde3f4cc36afc2d4210ddc5e7e6ac3d
hoan.nguyenduy <hoanduy1710@gmail.com>
2023-01-15 13:18:55 +0700

    add api user by id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,6 +64,39 @@ class UserController extends Controller
         return response()->json($user->public_key, 200);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/v2/api/users/{id}/public-key",
+     *     tags={"users v2"},
+     *     description="User uid",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string",
+     *              example="1",
+     *          )
+     *     ),
+     *     @OA\Response(response=200, description="Trả về public key")
+     * )
+     */
+    protected function getPublicKeyById(string $id): \Illuminate\Http\JsonResponse
+    {
+        try {
+            $user = User::where('id', $id)->firstOrFail();
+        } catch (Throwable $e) {
+            report($e);
+            return response()->json([
+                'error' => 'id không tồn tại'
+            ], 404);
+        }
+
+        return response()->json($user->public_key, 200);
+    }
+
+
+
     // protected function get(string $user_id): \Illuminate\Http\JsonResponse
     // {
     //     try {
@@ -110,6 +143,37 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Get(
+     *     path="/v2/api/users/{id}",
+     *     tags={"users v2"},
+     *     description="User uid",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string",
+     *              example="1",
+     *          )
+     *     ),
+     *     @OA\Response(response=200, description="Trả về user token")
+     * )
+     */
+    protected function getById(string $id): \Illuminate\Http\JsonResponse
+    {
+        try {
+            $user = User::where('id', $id)->firstOrFail();
+        } catch (Throwable $e) {
+            report($e);
+            return response()->json([
+                'error' => 'User Uid không tồn tại'
+            ], 404);
+        }
+
+        return response()->json($user, 200);
+    }
+
+    /**
      * @OA\Post (
      *     path="/api/users/verify-signature",
      *     tags={"users"},
@@ -133,6 +197,38 @@ class UserController extends Controller
     protected function verifySignature(Request $request): \Illuminate\Http\JsonResponse
     {
         $user = User::firstWhere('user_id', $request->input('user_id'));
+
+        if ($user == null) {
+            return response()->json(false, 200);
+        }
+
+        return response()->json(true, 200);
+    }
+
+    /**
+     * @OA\Post (
+     *     path="/v2/api/users/verify-signature",
+     *     tags={"users v2"},
+     *     description="User",
+     *     @OA\Response(response=200, description="Trả về true hoặc false thể hiện signutare có hợp lệ hay không"),
+     *     @OA\RequestBody(
+     *          description="Create user object",
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/VerifySignatureBody_v2")
+     *     )
+     * )
+     */
+     /**
+     * @OA\Schema(
+     *   schema="VerifySignatureBody_v2",
+     *   description="Verify Signature Body",
+     *   @OA\Property(property="id", type="string", description="id của user", example="1", nullable=false),
+     *   @OA\Property(property="signature", type="string", description="Loại token", example="", nullable=false),
+     * )
+     */
+    protected function verifySignatureById(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $user = User::firstWhere('id', $request->input('id'));
 
         if ($user == null) {
             return response()->json(false, 200);
@@ -176,6 +272,40 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Put (
+     *     path="/v2/api/users/{id}",
+     *     tags={"users v2"},
+     *     operationId="updateUser",
+     *     description="Cập nhật thông tin của User",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string",
+     *              example="1",
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="Trả về true hoặc false thể hiện thao tác thành công hay không"
+     *      ),
+     *     @OA\RequestBody(
+     *          description="Update user object",
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/UpdateUserSchema")
+     *     )
+     * )
+     */
+    protected function updateById(Request $request, string $id): \Illuminate\Http\JsonResponse
+    {
+        $user = User::where('id', $id)->firstOrFail();
+        $user->update($request->all());
+
+        return response()->json($user, 200);
+    }
+
+    /**
      * @OA\Delete (
      *     path="/api/users/{user_id}",
      *     tags={"users"},
@@ -199,6 +329,35 @@ class UserController extends Controller
     protected function delete(string $user_id): \Illuminate\Http\JsonResponse
     {
         $user = User::where('user_id', $user_id)->firstOrFail();
+        $user->delete();
+
+        return response()->json(true, 204);
+    }
+
+    /**
+     * @OA\Delete (
+     *     path="/v2/api/users/{id}",
+     *     tags={"users v2"},
+     *     operationId="deleteUser",
+     *     description="Xoá user",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string",
+     *              example="1",
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=204,
+     *          description="No content"
+     *      ),
+     * )
+     */
+    protected function deleteById(string $id): \Illuminate\Http\JsonResponse
+    {
+        $user = User::where('id', $id)->firstOrFail();
         $user->delete();
 
         return response()->json(true, 204);
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,12 @@ Route::put('/users/{user_id}', [UserController::class, 'update']);
 Route::delete('/users/{user_id}', [UserController::class, 'delete']);
 Route::post('/users/verify-signature', [UserController::class, 'verifySignature']);
 
+Route::get('/v2/users/{id}/public-key', [UserController::class, 'getPublicKeyById']);
+Route::get('/v2/users/{id}', [UserController::class, 'getById']);
+Route::put('/v2/users/{id}', [UserController::class, 'updateById']);
+Route::delete('/v2/users/{id}', [UserController::class, 'deleteById']);
+Route::post('/v2/users/verify-signature', [UserController::class, 'verifySignatureById']);
+
 Route::get('/key-encryption', [KeyEncryptionUserController::class, 'list']);
 Route::get('/key-encryption/rooms/{room_id}', [KeyEncryptionUserController::class, 'getByRoomId']);
 Route::get('/key-encryption/rooms/{room_id}/users/{user_id}', [KeyEncryptionUserController::class, 'getByUserId']);
